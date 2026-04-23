b92c1aadebc692514be6bb4a25cf0d6d5bda8d99
Connor Barnsley <connorbarnsley@Mac.lan>
2026-04-23 16:30:24 -0400

    vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import Link from "next/link";
 import "./globals.css";
 
